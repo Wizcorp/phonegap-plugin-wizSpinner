ec7a205d27e0c951a3e8f6f66fc1ec63725f1c38
aogilvie <aogilvie@wizcorp.jp>
2013-09-03 19:45:49 +0900

    fixed correct path for example project

--- example/www/cordova_plugins.js
+++ example/www/cordova_plugins.js
@@ -1,7 +1,7 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/jp.wizcorp.phonegap.plugin.wizSpinner.wizSpinnerPlugin/www/phonegap/plugin/wizSpinner/wizSpinner.js",
+        "file": "phonegap/plugin/wizSpinner/wizSpinner.js",
         "id": "jp.wizcorp.phonegap.plugin.wizSpinner.wizSpinnerPlugin.wizSpinnerPlugin",
         "clobbers": [
             "window.wizSpinner"
