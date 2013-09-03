1d76a48978178d9c92056cf1dd64699d5456a1fc
aogilvie <aogilvie@wizcorp.jp>
2013-09-03 18:45:35 +0900

    updated android for plugman and CDV 3.0

--- example/android/assets/www/phonegap/plugin/wizSpinner/wizSpinner.js
+++ example/android/assets/www/phonegap/plugin/wizSpinner/wizSpinner.js
@@ -7,83 +7,81 @@
  */
 
 /*
-============= Example rotation code ====================
+ ============= Example rotation code ====================
 
-function shouldRotateToOrientation (orientation) {
-	if (deviceIsReady == true) {
-		switch (orientation) {
-			case 0:
-				// portrait normal
-				window.wizSpinner.rotate(1);
-				return true;
-			break;
-			case 90:
-				// landscape left
-				window.wizSpinner.rotate(3);
-				return true;
-			break;
-			case -90:
-				// landscape right
-				window.wizSpinner.rotate(4);
-				return true;
-			break;
-			case 180:
-				// portrait upside down
-				return false;
-			break;
-		}
-	}
-}
+ function shouldRotateToOrientation (orientation) {
+ if (deviceIsReady == true) {
+ switch (orientation) {
+ case 0:
+ // portrait normal
+ window.wizSpinner.rotate(1);
+ return true;
+ break;
+ case 90:
+ // landscape left
+ window.wizSpinner.rotate(3);
+ return true;
+ break;
+ case -90:
+ // landscape right
+ window.wizSpinner.rotate(4);
+ return true;
+ break;
+ case 180:
+ // portrait upside down
+ return false;
+ break;
+ }
+ }
+ }
 
-function onDeviceReady() {
-	deviceIsReady = true;
-}
-*/
+ function onDeviceReady() {
+ deviceIsReady = true;
+ }
+ */
 
+var exec = require("cordova/exec");
 
-// For Cordova >= 3.0
-(function (window) {
+var WizSpinner = function () {
+};
 
-	// Object stringifier helper
-	function propsToString(obj) {
-		// stringify all vars
-		for (var i in obj) {
-            if (obj.hasOwnProperty(i)) {
-                obj[i] = '' + obj[i];
-            }
+WizSpinner.prototype.throwError = function (cb, error) {
+    if (cb) {
+        cb(error);
+    } else {
+        throw error;
+    }
+};
+
+// Object stringifier helper
+WizSpinner.prototype.propsToString = function (obj) {
+    // Stringify all vars
+    for (var i in obj) {
+        if (obj.hasOwnProperty(i)) {
+            obj[i] = '' + obj[i];
         }
     }
-    
-	// WizSpinner parent class
-	function WizSpinner() {	};
-	
-	WizSpinner.prototype.throwError = function (cb, error) {
-		if (cb) {
-			cb(error);
-		} else {
-			throw error;
-		}
-	};
-	
-	WizViewManager.prototype.create = function (options) {
-		propsToString(options);
-		cordova.exec(win, fail, 'WizSpinnerPlugin', 'create', [options]);
-	};
-	
-	WizViewManager.prototype.show = function (options) {
-		propsToString(options);
-		cordova.exec(null, null, 'WizSpinnerPlugin', 'show', [options]);
-	};
+}
 
-	WizViewManager.prototype.hide = function (options) {
-		propsToString(options);
-		cordova.exec(null, null, 'WizSpinnerPlugin', 'hide', [options]);
-	};
-	
-	WizViewManager.prototype.rotate = function (orientation) {
-		cordova.exec(null, null, 'WizSpinnerPlugin', 'rotate', [orientation]);
-	};
-		
-	// Instantiate the wizSpinner
-	window.wizSpinner = new WizSpinner();
-}(window));
+WizSpinner.prototype.create = function (options) {
+    this.propsToString(options);
+    exec(win, fail, 'WizSpinnerPlugin', 'create', [options]);
+};
+
+WizSpinner.prototype.show = function (options) {
+    this.propsToString(options);
+    exec(null, null, 'WizSpinnerPlugin', 'show', [options]);
+};
+
+WizSpinner.prototype.hide = function (options) {
+    this.propsToString(options);
+    exec(null, null, 'WizSpinnerPlugin', 'hide', [options]);
+};
+
+WizSpinner.prototype.rotate = function (orientation) {
+    exec(null, null, 'WizSpinnerPlugin', 'rotate', [orientation]);
+};
+
+// Instantiate the wizSpinner
+var wizSpinner = new WizSpinner();
+module.exports = wizSpinner;
--- www/phonegap/plugin/wizSpinner/wizSpinner.js
+++ www/phonegap/plugin/wizSpinner/wizSpinner.js
@@ -1,4 +1,4 @@
-cordova.define("jp.wizcorp.phonegap.plugin.wizSpinner.wizSpinnerPlugin.wizSpinnerPlugin", function(require, exports, module) {/* Spinner/Loader Cordova Plugin - JavaScript side of the bridge to iOS
+/* Spinner/Loader Cordova Plugin - JavaScript side of the bridge to iOS
  *
  * @author Ally Ogilvie
  * @copyright Wizcorp Inc. [ Incorporated Wizards ] 2013
@@ -84,4 +84,4 @@ WizSpinner.prototype.rotate = function (orientation) {
 
 // Instantiate the wizSpinner
 var wizSpinner = new WizSpinner();
-module.exports = wizSpinner;});
+module.exports = wizSpinner;
