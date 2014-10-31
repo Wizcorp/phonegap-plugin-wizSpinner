230310367d6bec482caa3cab0b35411a5a5cd314
aogilvie <aogilvie@wizcorp.jp>
2014-10-31 12:23:43 +0900

    updated sample ios project to 3.6

--- platforms/ios/CordovaLib/Classes/CDVAvailability.h
+++ platforms/ios/CordovaLib/Classes/CDVAvailability.h
@@ -47,6 +47,12 @@
 #define __CORDOVA_3_1_0 30100
 #define __CORDOVA_3_2_0 30200
 #define __CORDOVA_3_3_0 30300
+#define __CORDOVA_3_4_0 30400
+#define __CORDOVA_3_4_1 30401
+#define __CORDOVA_3_5_0 30500
+#define __CORDOVA_3_6_0 30600
+#define __CORDOVA_3_6_1 30601
+#define __CORDOVA_3_6_3 30603
 #define __CORDOVA_NA 99999      /* not available */
 
 /*
@@ -57,7 +63,7 @@
  #endif
  */
 #ifndef CORDOVA_VERSION_MIN_REQUIRED
-    #define CORDOVA_VERSION_MIN_REQUIRED __CORDOVA_3_3_0
+    #define CORDOVA_VERSION_MIN_REQUIRED __CORDOVA_3_6_3
 #endif
 
 /*
